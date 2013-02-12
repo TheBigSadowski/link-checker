ce2432d6f5f1443066ef009120f4bf01b7e58a59
TheBigSadowski <joe.sadowski@gmail.com>
2013-02-11 22:33:07 -0800

    removing more commented out code and some unused require statements

--- check-links.js
+++ check-links.js
@@ -1,18 +1,4 @@
-var lazy = require("lazy");
-var fs = require("fs");
 var http = require('http');
-
-/*
-new lazy(fs.createReadStream('links-to-check.txt'))
-    .lines
-    .forEach(function(line){
-        http.get(line.toString(), function(res) {
-           console.log(res.statusCode + ' ' + line); 
-        }).on('error', function(err){
-            console.log('ERROR!! ' + line);
-        });
-     });
-*/
 
 function check(link) {
 	var request = http.get(link.url, function(res) {
