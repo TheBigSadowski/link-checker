8d75cb0805f8c6de21cf836c4d5d365d7f8960e0
TheBigSadowski <joe.sadowski@gmail.com>
2013-02-20 14:49:33 -0800

    removing even more dead code

--- check-links.js
+++ check-links.js
@@ -46,43 +46,10 @@ var Monitor = function() {
 };
 util.inherits(Monitor, events.EventEmitter);
 
-/*
-function check(site, page, errors) {
-    var request = http.get({ hostname: site, path: page }, function(res) {
-        if (res.statusCode != 200) {
-            console.log(res.statusCode + '  ' + site + page);
-        } else {
-            res.setEncoding('utf8');
-            res.data = '';
-            res.on('data', function(chunk) {
-                res.data += chunk;
-            });
-            res.on('end', function() {
-                if (errors) for (var i = 0; i < errors.length; i++) {
-                    var rx = new RegExp(errors[i], "i");
-                    var fail = res.data.search(rx) >= 0;
-                    if (fail) {
-                        console.log("FAIL: " + site + page);
-                    }
-                }
-            });
-        }
-    });
-    request.on('error', function(err) {
-        console.log(err);
-    });
-}*/
 
 fs.readFile('links.json', function(err, data) {
     if (err) throw err;
     var sites = JSON.parse(data);
-    /*for (var i = 0; i < sites.length; i++) {
-        var site = sites[i];
-        for (var j = 0; j < site.pages.length; j++) {
-            check(site.site, site.pages[j], site.errors);
-        }
-    }*/
-
 	var monitor = new Monitor();
 	monitor.on('fail', function(why) { console.log(why); });
 	monitor.on('end', function() { /* checking complete! */ });
