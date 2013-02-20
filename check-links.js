bf737effa2765b96046dfd84a3d7f94987f153db
TheBigSadowski <joe.sadowski@gmail.com>
2013-02-20 15:17:19 -0800

    tabs => spaces

--- check-links.js
+++ check-links.js
@@ -4,10 +4,10 @@ var Monitor = require('./lib/monitor');
 fs.readFile('links.json', function(err, data) {
     if (err) throw err;
     var sites = JSON.parse(data);
-	var monitor = new Monitor();
-	monitor.on('fail', function(why) { console.log(why); });
-	monitor.on('ok', function(link) { console.log('Well, OK! ' + link.site + link.page); });
-	monitor.on('error', function(why) { console.log(why); });
-	monitor.on('end', function() { /* checking complete! */ });
-	monitor.check(sites);
+    var monitor = new Monitor();
+    monitor.on('fail', function(why) { console.log(why); });
+    monitor.on('ok', function(link) { console.log('Well, OK! ' + link.site + link.page); });
+    monitor.on('error', function(why) { console.log(why); });
+    monitor.on('end', function() { /* checking complete! */ });
+    monitor.check(sites);
 });
