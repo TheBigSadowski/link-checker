3f82eb882ec4bf8b9711bf6a24a8372cbdfbc5f3
TheBigSadowski <joe.sadowski@gmail.com>
2013-02-20 13:05:17 -0800

    Using new storage structure to store the pages and sites to check.

--- check-links.js
+++ check-links.js
@@ -1,32 +1,39 @@
 var http = require('http');
+var fs = require('fs');
 
-function check(link) {
-    var request = http.get(link.url, function(res) {
-        res.setEncoding('utf8');
-        res.data = '';
-        res.on('data', function(chunk) {
-            res.data += chunk;
-        });
-        res.on('end', function() {
-            if (link.failOnMatch) {
-                var rx = new RegExp(link.failOnMatch, "i");
-                var fail = res.data.search(rx) >= 0;
-                if (fail) {
-                    console.log("FAIL: " + link.url);
+function check(site, page, errors) {
+    var request = http.get({ hostname: site, path: page }, function(res) {
+        if (res.statusCode != 200) {
+            console.log(res.statusCode + '  ' + site + page);
+        } else {
+            res.setEncoding('utf8');
+            res.data = '';
+            res.on('data', function(chunk) {
+                res.data += chunk;
+            });
+            res.on('end', function() {
+                if (errors) for (var i = 0; i < errors.length; i++) {
+                    var rx = new RegExp(errors[i], "i");
+                    var fail = res.data.search(rx) >= 0;
+                    if (fail) {
+                        console.log("FAIL: " + site + page);
+                    }
                 }
-            }
-        });
+            });
+        }
     });
     request.on('error', function(err) {
         console.log(err);
     });
 }
 
-var links = [
- { "failOnMatch": "askldjf", "url": "http://nodejs.org/api/fs.html"},
- { "failOnMatch": "manual", "url": "http://nodejs.org/api/fs.html?"}
-];
-
-for (var i = 0; i < links.length; i++) {
-    check(links[i]);
-}
+fs.readFile('links.json', function(err, data) {
+    if (err) throw err;
+    var sites = JSON.parse(data);
+    for (var i = 0; i < sites.length; i++) {
+        var site = sites[i];
+        for (var j = 0; j < site.pages.length; j++) {
+            check(site.site, site.pages[j], site.errors);
+        }
+    }
+});
